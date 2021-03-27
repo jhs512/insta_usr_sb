2c802eb4e7d4bb444e250b418481f671b8fa5f98
jhs512 <jangka512@gmail.com>
2021-03-27 16:49:24 +0900

    첫 커밋

---
+++ src/main/java/com/sbs/untactTeacher/InstaApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.untactTeacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/untactTeacher/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.untactTeacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
