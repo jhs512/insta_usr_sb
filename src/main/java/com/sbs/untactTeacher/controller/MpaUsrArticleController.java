84a159cc9968b14367f7450d5adc505118ab3158
jhs512 <jangka512@gmail.com>
2021-03-28 16:30:30 +0900

    생성된 게시물을 보관했다가, 고객이 달라고 하면 준다.

--- src/main/java/com/sbs/untactTeacher/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untactTeacher/controller/MpaUsrArticleController.java
@@ -1,5 +1,8 @@
 package com.sbs.untactTeacher.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,23 +13,62 @@ import com.sbs.untactTeacher.util.Util;
 
 @Controller
 public class MpaUsrArticleController {
-	private int articleLastId = 0;
+	private List<Article> articles;
+	private int articleLastId;
+
+	public MpaUsrArticleController() {
+		articles = new ArrayList<>();
+		articleLastId = 0;
+	}
 
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
-		int id = articleLastId + 1;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-		
-		Article article = new Article(id, regDate, updateDate, title, body);
-
-		articleLastId = id;
+		int id = writeArticle(title, body);
+		Article article = getArticleById(id);
 
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
+
+	private int writeArticle(String title, String body) {
+		int id = articleLastId + 1;
+		String regDate = Util.getNowDateStr();
+		String updateDate = Util.getNowDateStr();
+
+		Article article = new Article(id, regDate, updateDate, title, body);
+		articles.add(article);
+
+		articleLastId = id;
+
+		return id;
+	}
+
+	@RequestMapping("/mpaUsr/article/getArticle")
+	@ResponseBody
+	public ResultData getArticle(int id) {
+		Article article = getArticleById(id);
+
+		if (article == null) {
+			return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", "id", id);
+		}
+
+		return new ResultData("S-1", article.getId() + "번 글 입니다.", "article", article);
+	}
+
+	private Article getArticleById(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		/*
+		 * for ( int i = 0; i < articles.size(); i++ ) { Article article =
+		 * articles.get(i);
+		 * 
+		 * if ( article.getId() == id ) { return article; } }
+		 */
+
+		return null;
+	}
 }
-
-
-
-
