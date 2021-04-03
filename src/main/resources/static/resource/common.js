a8ba8fb91bda13deec29f30496ebf6221f81c9a7
jhs512 <jangka512@gmail.com>
2021-04-03 17:55:01 +0900

    메인 화면 꾸미기, 공통 레이아웃 만들기

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -1,3 +1,8 @@
-function hello() {
-	alert('hi');
+function isTouchDevice() {
+  return (('ontouchstart' in window) ||
+          (navigator.maxTouchPoints > 0) ||
+          (navigator.msMaxTouchPoints > 0));
 }
+
+const $html = document.querySelector('html');
+$html.classList.add(isTouchDevice() ? 'touch-posible' : 'touch-imposible');
