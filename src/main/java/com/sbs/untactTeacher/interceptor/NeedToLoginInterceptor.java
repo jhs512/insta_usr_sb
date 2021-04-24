fa890c8a3dcf8723df9e74b3c79acd33b07a0109
chacha86 <taejincha0619@gmail.com>
2021-04-24 15:28:56 +0900

    로그인이 안된 상태에서 글작성 페이지로 이동시 로그인 페이지로 리다이렉트

--- src/main/java/com/sbs/untactTeacher/interceptor/NeedToLoginInterceptor.java
+++ src/main/java/com/sbs/untactTeacher/interceptor/NeedToLoginInterceptor.java
@@ -18,7 +18,7 @@ public class NeedToLoginInterceptor implements HandlerInterceptor {
 
         if (rq.isNotLogined()) {
             resp.setContentType("text/html; charset=UTF-8");
-            resp.getWriter().append(Util.msgAndBack("로그인 후 이용해주세요."));
+            resp.getWriter().append(Util.msgAndReplace("로그인 후 이용해주세요.", "../member/login"));
             return false;
         }
 
